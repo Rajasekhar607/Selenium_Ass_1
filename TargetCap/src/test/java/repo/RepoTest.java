374409774e9cf63e0f2764ad7b64d2825fd1113d
gunas <gunas@LAPTOP-GEIVF1FS>
2024-11-16 11:34:27 +0530

    Assignment Push

---
+++ TargetCap/src/test/java/repo/RepoTest.java
@@ -0,0 +1,11 @@
+package repo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class RepoTest {
+@Test
+public void demo() {
+	Reporter.log("Repo class executed",true);
+}
+}
